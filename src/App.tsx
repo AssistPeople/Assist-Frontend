59936ed4953abef9df2b11ff7e589bdb4f1a43fc
ZUITOPIA <korhwngml123@naver.com>
2025-02-20 14:34:23 +0900

    feat: GDS에 존재하지 않는 컴포넌트 UI 파일에 분리

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import GlobalStyle from "./styles/GlobalStyles";
+import ProfileSetUpPage from "./pages/ProfileSetUpPage";
 
 const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
     { path: "/login", element: <LoginPage /> },
+    { path: "/profile-setup", element: <ProfileSetUpPage /> },
 ]);
 
 function App() {
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,8 @@ const HomePage = () => {
             <h1>Home Page</h1>
             <p>홈페이지입니다.</p>
             <Link to="/login">로그인 페이지로 이동</Link>
+
+            <Link to="/profile-setup">프로필 등록하러 가기 (임시)</Link>
         </div>
     );
 };
