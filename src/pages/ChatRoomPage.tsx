0084975f55613e4de44c361f0243c06684f703f1
고주희 <zuitopia@zuitopia.local>
2025-02-21 02:10:41 +0900

    feat: 채팅 기본 정보 생성

--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -1,16 +1,36 @@
 import styled from "styled-components";
 import Header from "../components/layout/Header";
 import { Style } from "../styles/UI";
-import { IconButton } from "@goorm-dev/vapor-core";
-import { SendIcon } from "@goorm-dev/vapor-icons";
-// import { IconButton, SendIcon } from "@goorm-dev/vapor-core";
+import { IconButton, Text } from "@goorm-dev/vapor-core";
+import { SendIcon, LocationIcon } from "@goorm-dev/vapor-icons";
 
 export default function ChatRoomPage() {
     return (
         <>
             <Header prefix="backButton" title="채팅" />
 
-            <Style.Container>ddd</Style.Container>
+            <Style.Container>
+                {/* 게스트하우스 정보 */}
+                <ChatInfoItemWrapper>
+                    <ProfileImage src="/profile.svg" alt="게스트하우스 프로필" />
+
+                    <ProfileInfo>
+                        <Text typography="heading6">필릿 게스트하우스</Text>
+
+                        <Location>
+                            <LocationIcon style={{ color: "var(--orange-500)" }} />
+                            <Text typography="body3" style={{ color: "var(--gray-500)" }}>
+                                함덕 해수욕장
+                            </Text>
+                        </Location>
+                    </ProfileInfo>
+                    <EtcInfo>
+                        <Text typography="body4">열람함</Text>
+                    </EtcInfo>
+                </ChatInfoItemWrapper>
+                <Style.Divider />
+                {/* 채팅 내용 */}
+            </Style.Container>
 
             <Style.FloatBox>
                 <SendMessageBoxWrapper>
@@ -37,4 +57,40 @@ const SendMessageBoxWrapper = styled.div`
 
 const InputText = styled.input`
     width: 260px;
+`;
+
+const ChatInfoItemWrapper = styled.div`
+    width: 100%;
+    height: 52.46px;
+    display: flex;
+    padding-bottom: 12px;
+    justify-content: space-between;
+    gap: 13px;
+`;
+
+const ProfileImage = styled.img`
+    width: 52.46px;
+    padding-top: 4px;
+`;
+
+const ProfileInfo = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const Location = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`;
+
+const EtcInfo = styled.div`
+    width: 70px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-end;
 `;
