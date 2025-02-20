c61be116c519a6f6f96d23973d5849289c578693
hoon-hoon <kc0393@naver.com>
2025-02-21 08:55:20 +0900

    style: backBtn 수정

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -25,9 +25,10 @@ export default function Header({ prefix, title, onclick }: Props) {
                     <IconButton
                         onClick={onclick || handleBackButtonClick}
                         aria-label="submit"
-                        size="md"
+                        size="lg"
                         color="secondary"
-                        shape="outline"
+                        shape="invisible"
+                        style={{ border: "none" }}
                     >
                         <ChevronLeftOutlineIcon />
                     </IconButton>
