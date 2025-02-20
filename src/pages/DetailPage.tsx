5b610c352eba739b2dfffc7b0777ea452bf87a66
hoon-hoon <kc0393@naver.com>
2025-02-21 05:51:35 +0900

    style: DetailPage 스타일 수정

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -172,7 +172,9 @@ const ButtonBox = styled.div`
     display: flex;
     justify-content: space-between;
     gap: 8px;
-    margin-top: 12px;
+    position: relative;
+    bottom: 0;
+    margin-bottom: 20px;
 `;
 
 const TextContainer = styled.div`
