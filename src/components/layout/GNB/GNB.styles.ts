fa483ad78c185174bbc320214a5d127b1cf5c4f7
고주희 <zuitopia@zuitopia.local>
2025-02-21 05:43:16 +0900

    style: GNB 설정 수정

--- src/components/layout/GNB/GNB.styles.ts
+++ src/components/layout/GNB/GNB.styles.ts
@@ -9,10 +9,13 @@ export const Container = styled.div`
     width: 393px;
     height: 72px;
     padding: 10px 84px;
+    position: fixed;
+    background-color: white;
     bottom: 0;
     box-shadow: 0px -3px 10px -2px rgba(0, 0, 0, 0.2);
     -webkit-box-shadow: 0px -3px 10px -2px rgba(0, 0, 0, 0.2);
     -moz-box-shadow: 0px -3px 10px -2px rgba(0, 0, 0, 0.2);
+    /* border: 1px solid red; */
 `;
 
 export const MenuItem = styled.div<{ isActive: boolean }>`
