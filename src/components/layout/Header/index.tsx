7b4856df878a1c450a42dd356b6e90fb7351a960
hoon-hoon <kc0393@naver.com>
2025-02-21 06:07:42 +0900

    feat: 헤더컴포넌트 수정

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -3,10 +3,11 @@ import { StyleHeader, Title } from "./Header.styles.ts";
 import { useNavigate } from "react-router-dom"; // navigate 함수 사용을 위해 import
 import { DarkIcon } from "@goorm-dev/vapor-icons";
 import { ChevronLeftOutlineIcon } from "@goorm-dev/vapor-icons";
+import { ReactNode } from "react";
 
 interface Props {
     prefix?: "backButton" | "logo";
-    title?: string; // 중앙 제목
+    title?: string | ReactNode;
     onclick?: () => void; // 뒤로가기 버튼 클릭 시 실행할 함수
 }
 
@@ -21,26 +22,21 @@ export default function Header({ prefix, title, onclick }: Props) {
         <StyleHeader>
             {prefix ? (
                 prefix === "backButton" ? (
-                    <>
-                        <IconButton
-                            onClick={onclick || handleBackButtonClick}
-                            aria-label="submit"
-                            size="md"
-                            // color="background-normal"
-                            style={{ color: "var(--gray-900)" }}
-                        >
-                            <ChevronLeftOutlineIcon />
-                        </IconButton>
-                    </>
+                    <IconButton
+                        onClick={onclick || handleBackButtonClick}
+                        aria-label="submit"
+                        size="md"
+                        color="secondary"
+                        shape="outline"
+                    >
+                        <ChevronLeftOutlineIcon />
+                    </IconButton>
                 ) : (
                     <DarkIcon />
                 )
             ) : null}
-            {title && (
-                <Title>
-                    <Text typography="heading5">{title}</Text>
-                </Title>
-            )}
+
+            {title && <Title>{typeof title === "string" ? <Text typography="heading5">{title}</Text> : title}</Title>}
         </StyleHeader>
     );
 }
